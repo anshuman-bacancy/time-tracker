032c66a5d143397a47a9255ffabf5a3d3b65001f
Anshuman Aich <anshuman.aich@bacancy.com>
2021-10-10 00:25:25 +0530

    changed todo note asthetics

--- src/Todos/Todo/todo.js
+++ src/Todos/Todo/todo.js
@@ -10,29 +10,8 @@ function Todo({todo}) {
 
   return (
     <>
-    <div className="row" style={{width: "300px", marginRight: "20px"}} draggable onDragStart={(e) => dragStart(e, todo)}>
-      <div className="col s6 m6">
-        <div className="sticky-container">
-          <div className="sticky-outer">
-            <div className="sticky">
-              <svg width="0" height="0">
-                <defs>
-                  <clipPath id="stickyClip" clipPathUnits="objectBoundingBox">
-                  <path
-                    d="M 0 0 Q 0 0.69, 0.03 0.96 0.03 0.96, 1 0.96 Q 0.96 0.69, 0.96 0 0.96 0, 0 0"
-                    stroke-linejoin="round"
-                    stroke-linecap="square"
-                  />
-                  </clipPath>
-                </defs>
-              </svg>
-              <div className="sticky-content">
-                {todo.msg}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="todoNote" draggable onDragStart={(e) => dragStart(e, todo)}>
+      <div>{todo.msg}</div>
     </div>
     </>
   )
