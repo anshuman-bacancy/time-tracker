601b6d49d5477abb7156a1fe1f5ea7112f268f48
Anshuman Aich <anshuman.aich@bacancy.com>
2021-10-18 16:39:15 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Login/login';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import InfoPanel from './InfoPanel/infoPanel';
+import Header from './Header/header';
+import Todos from './Todos/todos';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => {
+          return (
+            <Fragment>
+              <Header />
+              <Login />
+            </Fragment>
+          )
+        }} 
+        />
+        <Route exact path="/dash" render={() => { 
+          return (
+            <Fragment>
+              <Header />
+              <InfoPanel />
+              <Todos />
+            </Fragment>
+          )
+        }}
+        />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
