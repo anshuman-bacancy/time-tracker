7c5332c552baae2481f10962e49e0017c29a0a67
Anshuman Aich <anshuman.aich@bacancy.com>
2022-02-11 18:18:52 +0530

    removed time and date code

--- src/InfoPanel/MiscInfo/miscInfo.js
+++ src/InfoPanel/MiscInfo/miscInfo.js
@@ -9,27 +9,20 @@ function MiscInfo() {
   useEffect(() => {
     setUser(localStorage.getItem("userName"))
     setUserImg(localStorage.getItem("userImg"))
-
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-    var finalDate = dd + '/' + mm + '/' + yyyy;
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    setCurrDate(finalDate);
-    setCurrTime(time);
+    setCurrDate(localStorage.getItem("currDate"));
+    setCurrTime(localStorage.getItem("loginTime"));
   }, []);
 
   return (
     <>
     <div className="card" style={{width: "20rem"}}>
-      {/* <img className="card-img-top" style={{height: "50px", width: "50px"}} src={userImg} alt=""></img> */}
       <div className="card-body">
         <div className="row">
           <div className="col">
             <p className="card-text">{user}</p>
             <p>{currDate}</p>
             <span>Logged in @ {currTime}</span>
+            <div><span>Timer started @ {localStorage.getItem("timerStart")}</span></div>
           </div>
           <img className="card-img-top" style={{height: "100px", width: "100px"}} src={userImg} alt=""></img>
         </div>
