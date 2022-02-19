a8676e670c61364e0ec6ce9f54484441d4e4569d
Anshuman Aich <anshuman.aich@bacancy.com>
2022-02-20 00:19:53 +0530

    change in import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from './Login/login';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import InfoPanel from './InfoPanel/infoPanel';
 import Header from './Header/header';
-import Todos from './Todos/todos';
+import Todos from './Todos/Todos';
 import { Fragment } from 'react';
 
 function App() {
